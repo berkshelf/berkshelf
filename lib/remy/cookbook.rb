8ad6cbcccaad0dcb7631aa45afd95ed72942923a
Josiah Kiehl <josiah@skirmisher.net>
2012-04-30 20:22:17 -0700

    Download location constant

--- lib/remy/cookbook.rb
+++ lib/remy/cookbook.rb
@@ -1,13 +1,14 @@
 class Cookbook
   attr_reader :name, :version_constraint
 
+  DOWNLOAD_LOCATION = '/tmp'
+
   def initialize name, version_constraint=nil
     @name = name
     @version_constraint = version_constraint
   end
 
   def download version
-    Chef::Knife::CookbookSiteDownload.new([name, '--file', File.join('/tmp', "#{name}-#{version}.tar.gz").run
+    Chef::Knife::CookbookSiteDownload.new([name, '--file', File.join(DOWNLOAD_LOCATION, "#{name}-#{version}.tar.gz")]).run
   end
-
 end
