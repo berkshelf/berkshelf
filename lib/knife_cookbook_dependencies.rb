0551cd1f81390319835777e7661704a007fe2e17
Jamie Winsor <jamie@vialstudios.com>
2012-05-13 20:32:02 -0700

    remove alias require from lib entry point

--- lib/knife_cookbook_dependencies.rb
+++ lib/knife_cookbook_dependencies.rb
@@ -4,7 +4,6 @@ require 'archive/tar/minitar'
 require 'chef/config'
 require 'chef/knife/cookbook_site_download'
 
-require 'knife_cookbook_dependencies/alias'
 require 'knife_cookbook_dependencies/version'
 require 'knife_cookbook_dependencies/shelf'
 require 'knife_cookbook_dependencies/cookbook'
