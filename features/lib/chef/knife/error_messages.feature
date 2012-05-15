6a5b09a589057f3a0c9c1f9fcb63d057592b56be
Josiah Kiehl <josiah@skirmisher.net>
2012-05-15 14:20:53 -0700

    Rewrite spec as feature for missing cookbook error message

--- features/lib/chef/knife/error_messages.feature
+++ features/lib/chef/knife/error_messages.feature
@@ -6,3 +6,11 @@ Feature: Friendly error messages
   Scenario: running without a Cookbookfile
     When I run `knife cookbook dependencies install`
     Then the output should contain "FATAL: There is no Cookbookfile in "
+
+  Scenario: when missing a cookbook
+    Given I write to "Cookbookfile" with:
+    """
+    cookbook "doesntexist"
+    """
+    When I run `knife cookbook dependencies install`
+    Then the output should contain "FATAL: The cookbook doesntexist was not found on the Opscode Community site. Provide a git or path key for doesntexist if it is unpublished."
--- spec/acceptance/knife_cookbook_dependencies_spec.rb
+++ spec/acceptance/knife_cookbook_dependencies_spec.rb
@@ -7,16 +7,6 @@ describe "knife cookbook dependencies install" do
   describe "should print a friendly error message" do
     include Aruba::Api
 
-    it "for missing cookbooks" do
-      pending "Knife commands produce no output when run with aruba. I'm not sure why." # TODO FIXME
-
-      cookbook_name = 'thisisamissingcookbook'
-      with_cookbookfile %Q[cookbook "#{cookbook_name}"] do
-        cmd = 'cat nofile' #'knife cookbook dependencies install'
-        process = run(cmd)
-        process.output(true).should match(/#{KCD::ErrorMessages.missing_cookbook(cookbook_name)}/)
-      end
-    end
     it "for --without option"
   end
 end
