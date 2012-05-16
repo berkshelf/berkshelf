92fcea59aa6b9d9d8ab0145034e7bacb31ec8c01
jhowarth <jhowarth@riotgames.com>
2012-05-15 17:22:17 -0700

    Added comment for clarity.

--- spec/lib/kcd/cookbook_spec.rb
+++ spec/lib/kcd/cookbook_spec.rb
@@ -88,6 +88,7 @@ module KnifeCookbookDependencies
       it "should compute the correct dependencies" do
         cookbook = Cookbook.new('mysql')
         cookbook.dependencies.should == [Cookbook.new('openssl')]
+        # Second computation is intentional, to make sure it doesn't change the dependency list.
         cookbook.dependencies.should == [Cookbook.new('openssl')]
       end
     end
