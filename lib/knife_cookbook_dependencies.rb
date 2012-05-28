c5c7207bc72110b804e968d65f167335a2d64e59
Jamie Winsor <jamie@vialstudios.com>
2012-05-27 19:56:27 -0700

    add helper function for finding the metadata of a cookbook by
    ascending up from a given path

--- lib/knife_cookbook_dependencies.rb
+++ lib/knife_cookbook_dependencies.rb
@@ -37,6 +37,21 @@ module KnifeCookbookDependencies
       FileUtils.rm_rf COOKBOOKS_DIRECTORY
       FileUtils.rm_rf TMP_DIRECTORY
     end
+
+    # Ascend the directory structure from the given path to find a
+    # metadata.rb file of a Chef Cookbook. If no metadata.rb file
+    # was found, nil is returned.
+    #
+    # @returns[Pathname] 
+    #   path to metadata.rb 
+    def find_metadata(path = Dir.pwd)
+      path = Pathname.new(path)
+      path.ascend do |potential_root|
+        if potential_root.entries.collect(&:to_s).include?('metadata.rb')
+          return potential_root.join('metadata.rb')
+        end
+      end
+    end
   end
 end
 
