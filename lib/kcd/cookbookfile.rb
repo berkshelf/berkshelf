25ebd5535684268931acf018f536642919e38fba
Jamie Winsor <jamie@vialstudios.com>
2012-06-17 19:24:44 -0700

    refactor Cookbookfile functions and add documentation and tests

--- lib/kcd/cookbookfile.rb
+++ lib/kcd/cookbookfile.rb
@@ -16,29 +16,88 @@ module KnifeCookbookDependencies
 
         object
       end
+
+      # @param [Array] sources
+      #   an array of sources to filter
+      # @param [Array, Symbol] excluded
+      #   an array of symbols or a symbol representing the group or group(s)
+      #   to exclude
+      #
+      # @return [Array<KCD::CookbookSource>]
+      #   an array of sources that are not members of the excluded group(s)
+      def filter_sources(sources, excluded)
+        excluded = Array(excluded)
+        excluded.collect!(&:to_sym)
+
+        sources.select { |source| (excluded & source.groups).empty? }
+      end
     end
 
     def initialize
       @sources = Hash.new
     end
 
+    # Add the given source to the sources array. A DuplicateSourceDefined
+    # exception will be raised if a source is added whose name conflicts
+    # with a source who has already been added.
+    #
+    # @param [KCD::CookbookSource] source
+    #   the source to add
+    #
+    # @return [Array<KCD::CookbookSource]
     def add_source(source)
       raise DuplicateSourceDefined if has_source?(source)
       @sources[source.to_s] = source
     end
 
+    # @param [#to_s] source
+    #   the source to remove
+    #
+    # @return [KCD::CookbookSource]
     def remove_source(source)
       @sources.delete(source.to_s)
     end
 
+    # @param [#to_s] source
+    #   the source to check presence of
+    #
+    # @return [Boolean]
     def has_source?(source)
       @sources.has_key?(source.to_s)
     end
 
-    def sources
-      @sources.collect { |name, source| source }.flatten
+    # @param [Hash] options
+    #   a hash of options
+    #
+    #   Options:
+    #     exclude: An array of groups to exclude from the returned Array
+    #       of sources
+    #
+    # @return [Array<KCD::CookbookSource>]
+    def sources(options = {})
+      l_sources = @sources.collect { |name, source| source }.flatten
+
+      if options[:exclude]
+        self.class.filter_sources(l_sources, options[:exclude])
+      else
+        l_sources
+      end
     end
 
+    # @return [Hash]
+    #   a hash containing group names as keys and an array of CookbookSources
+    #   that are a member of that group as values
+    #
+    #   Example:
+    #     {
+    #       nautilus: [
+    #         #<KCD::CookbookSource @name="nginx">,
+    #         #<KCD::CookbookSource @name="mysql">,
+    #       ],
+    #       skarner: [
+    #         #<KCD::CookbookSource @name="nginx">
+    #       ]
+    #     }
     def groups
       {}.tap do |groups|
         @sources.each_pair do |name, source|
@@ -52,11 +111,19 @@ module KnifeCookbookDependencies
 
     # @param [String] name
     #   name of the source to return
+    #
+    # @return [KCD::CookbookSource]
     def [](name)
       @sources[name]
     end
     alias_method :get_source, :[]
 
+    # @param [Hash] options
+    #   a hash of options
+    #   
+    #   Options:
+    #     without: An array of groups to exclude which will cause any sources
+    #       marked as a member of the group to not be installed
     def install(options = {})
       if File.exist?(KCD::Lockfile::DEFAULT_FILENAME)
         filename = KCD::Lockfile::DEFAULT_FILENAME
@@ -66,45 +133,38 @@ module KnifeCookbookDependencies
         lockfile = false
       end
 
-      l_sources = if options[:without]
-        filter_sources(options[:without])
-      else
-        sources
-      end
-
-      resolver = Resolver.new(KCD.downloader, l_sources)
+      resolver = Resolver.new(KCD.downloader, sources(exclude: options[:without]))
       resolver.resolve
       write_lockfile(resolver.sources) unless lockfile
-
-      true
     end
 
+    # @param [String] chef_server_url
+    #   the full URL to the Chef Server to upload to
+    #
+    #   Example:
+    #     "https://api.opscode.com/organizations/vialstudios"
+    #
+    # @param [Hash] options
+    #   a hash of options
+    #
+    #   Options:
+    #     without: An array of groups to exclude which will cause any sources
+    #       marked as a member of the group to not be installed
+    #     force: Upload the Cookbook even if the version already exists and is
+    #       frozen on the target Chef Server
+    #     freeze: Freeze the uploaded Cookbook on the Chef Server so that it
+    #       cannot be overwritten
     def upload(chef_server_url, options = {})
-      l_sources = if options[:without]
-        filter_sources(options[:without])
-      else
-        sources
-      end
-
-      resolver = Resolver.new(KCD.downloader, l_sources)
-      cookbooks = resolver.resolve
-
       uploader = Uploader.new(KCD.cookbook_store, chef_server_url)
+      resolver = Resolver.new(KCD.downloader, sources(exclude: options[:without]))  
 
-      cookbooks.each do |name, version|
+      resolver.resolve.each do |name, version|
         KCD.ui.info "Uploading #{name} (#{version}) to: #{chef_server_url}"
-        uploader.upload!(name, version)
+        uploader.upload!(name, version, options)
       end
     end
 
     private
-
-      def filter_sources(excluded)
-        excluded.collect!(&:to_sym)
-        sources.select do |source|
-          (excluded & source.groups).empty?
-        end
-      end
 
       def write_lockfile(sources)
         KCD::Lockfile.new(sources).write
