f5c662f9b5b41f594e055303cb9371254c158a2a
Jamie Winsor <jamie@vialstudios.com>
2012-06-17 18:03:35 -0700

    add default option for --without flag on install

--- lib/chef/knife/cookbook_dependencies_install.rb
+++ lib/chef/knife/cookbook_dependencies_install.rb
@@ -12,12 +12,13 @@ module KnifeCookbookDependencies
       :short => "-W WITHOUT",
       :long => "--without WITHOUT",
       :description => "Exclude cookbooks that are in these groups",
-      :proc => lambda { |w| w.split(",") }
+      :proc => lambda { |w| w.split(",") },
+      :default => Array.new
 
     def run
       ::KCD.ui = ui
       cookbook_file = ::KCD::Cookbookfile.from_file(File.join(Dir.pwd, "Cookbookfile"))
-      cookbook_file.install(:without => config[:without])
+      cookbook_file.install(config)
     rescue KCDError => e
       KCD.ui.fatal e
       exit e.status_code
