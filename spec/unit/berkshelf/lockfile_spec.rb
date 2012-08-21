219a487a30172e8ec5d95a2dbcea6cb410fc749d
Jamie Winsor <jamie@vialstudios.com>
2012-08-21 12:51:34 -0700

    fix lockfile specs

--- spec/unit/berkshelf/lockfile_spec.rb
+++ spec/unit/berkshelf/lockfile_spec.rb
@@ -2,9 +2,14 @@ require 'spec_helper'
 
 describe Berkshelf::Lockfile do
   describe "without a lockfile in place already" do
-    before do
+    before(:all) do
       @old_dir = Dir.pwd
       Dir.chdir fixtures_path.join("lockfile_spec", "without_lock")
+    end
+
+    after(:all) do
+      FileUtils.rm(fixtures_path.join("lockfile_spec", "without_lock", "Berksfile.lock"))
+      Dir.chdir(@old_dir)
     end
 
     it "should be able to write a Berksfile.lock from a list of cookbooks" do
@@ -14,8 +19,8 @@ describe Berkshelf::Lockfile do
       Berkshelf::Lockfile.new(resolver.sources).write
 
       File.read('Berksfile.lock').split(/\r?\n/).sort.should == [
-        "cookbook 'bluepill', :locked_version => '1.0.6'",
-        "cookbook 'build-essential', :locked_version => '1.1.0'",
+        "cookbook 'bluepill', :locked_version => '1.0.4'",
+        "cookbook 'build-essential', :locked_version => '1.0.2'",
         "cookbook 'nginx', :locked_version => '0.101.0'",
         "cookbook 'ohai', :locked_version => '1.0.2'",
         "cookbook 'runit', :locked_version => '0.15.0'"
