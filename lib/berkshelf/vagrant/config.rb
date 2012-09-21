4406f899953f63104746d5deac22cdba91a58f67
Jamie Winsor <jamie@vialstudios.com>
2012-09-21 15:07:08 -0700

    fuck that advice

--- lib/berkshelf/vagrant/config.rb
+++ lib/berkshelf/vagrant/config.rb
@@ -2,42 +2,19 @@ module Berkshelf
   module Vagrant
     # @author Jamie Winsor <jamie@vialstudios.com>
     # @author Andrew Garson <andrew.garson@gmail.com>
-    #
-    # @note according to <http://vagrantup.com/v1/docs/extending/configuration.html> instance
-    #   variables should not be instantiated with default values in the initializer of a Config file.
-    #   Instead explicit getters which set and return the default value should be used.
     class Config < ::Vagrant::Config::Base
       # @return [String]
       #   path to a knife configuration file
-      def config_path
-        @config_path ||= Berkshelf::DEFAULT_CONFIG
-      end
-
-      # @param [String] value
-      def config_path=(value)
-        @config_path = File.expand_path(value)
-      end
+      attr_reader :config_path
 
       # @return [String]
       #   path to the Berksfile to use with Vagrant
-      def berksfile_path
-        @berksfile_path ||= File.join(Dir.pwd, Berkshelf::DEFAULT_FILENAME)
-      end
-
-      # @param [String] value
-      def berksfile_path=(value)
-        @berksfile_path = File.expand_path(value)
-      end
+      attr_reader :berksfile_path
 
       # @return [String]
       #   A path to a client key on disk to use with the Chef Client provisioner to
       #   upload cookbooks installed by Berkshelf.
       attr_reader :client_key
-
-      # @param [String] value
-      def client_key=(value)
-        @client_key = File.expand_path(value)
-      end
 
       # @return [String]
       #   A client name (node_name) to use with the Chef Client provisioner to upload
@@ -45,27 +22,40 @@ module Berkshelf
       attr_accessor :node_name
 
       # @return [Array<Symbol>]
-      #   cookbooks in all other groups except for these will be installed
-      #   and copied to Vagrant's shelf
-      def except
-        @except ||= Array.new
-      end
-      attr_writer :except
-
-      # @return [Array<Symbol>]
       #   only cookbooks in these groups will be installed and copied to
       #   Vagrant's shelf
-      def only
-        @only ||= Array.new
-      end
-      attr_writer :only
+      attr_accessor :only
 
-      # @return [Boolean]
-      #   should connections to a Chef API use SSL verification
-      def ssl_verify
-        @ssl_verify ||= true
+      # @return [Array<Symbol>]
+      #   cookbooks in all other groups except for these will be installed
+      #   and copied to Vagrant's shelf
+      attr_accessor :except
+      attr_accessor :ssl_verify
+
+      def initialize
+        @berksfile_path = File.join(Dir.pwd, Berkshelf::DEFAULT_FILENAME)
+        @config_path    = Berkshelf::DEFAULT_CONFIG
+        @node_name      = nil
+        @client_key     = nil
+        @except         = Array.new
+        @only           = Array.new
+        @ssl_verify     = true
       end
-      attr_writer :ssl_verify
+
+      # @param [String] value
+      def berksfile_path=(value)
+        @berksfile_path = File.expand_path(value)
+      end
+
+      # @param [String] value
+      def config_path=(value)
+        @config_path = File.expand_path(value)
+      end
+
+      # @param [String] value
+      def client_key=(value)
+        @client_key = File.expand_path(value)
+      end
 
       def validate(env, errors)
         if !except.empty? && !only.empty?
