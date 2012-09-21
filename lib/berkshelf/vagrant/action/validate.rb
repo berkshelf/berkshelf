9e34846d1d44f38bd8a6d6a1344c8ed7918576e5
Jamie Winsor <jamie@vialstudios.com>
2012-09-20 19:02:35 -0700

    fix uploads when using chef_client provisioner
    fix issue where config validations would not occur if a Vagrantfile did not
    explicitly include a berkshelf config key

---
+++ lib/berkshelf/vagrant/action/validate.rb
@@ -0,0 +1,30 @@
+module Berkshelf
+  module Vagrant
+    module Action
+      # @author Jamie Winsor <jamie@vialstudios.com>
+      #
+      # As of Vagrant 1.0.5 it is not possible to validate configuration values of
+      # a configuraiton that was not explicitly described in a Vagrant::Config.run block.
+      #
+      # In our case we want some values set for our middleware stacks even if the user does
+      # not explicitly set values for settings in `config.berkshelf`.
+      class Validate
+        def initialize(app, env)
+          @app = app
+        end
+
+        def call(env)
+          recorder = ::Vagrant::Config::ErrorRecorder.new
+          env[:vm].config.berkshelf.validate(env[:vm].env, recorder)
+          
+          unless recorder.errors.empty?
+            raise ::Vagrant::Errors::ConfigValidationFailed, 
+              messages: ::Vagrant::Util::TemplateRenderer.render("config/validation_failed", errors: { berkshelf: recorder })
+          end
+
+          @app.call(env)          
+        end
+      end
+    end
+  end
+end
