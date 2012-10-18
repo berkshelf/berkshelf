ad3e8f0012fcb9994add02ad43ccd1d1b8e74433
Justin Campbell <justin@justincampbell.me>
2012-10-18 17:37:42 -0400

    Do not inspect InvalidConfig error keys

--- lib/berkshelf/errors.rb
+++ lib/berkshelf/errors.rb
@@ -84,8 +84,20 @@ module Berkshelf
   class InvalidConfiguration < BerkshelfError
     status_code(115)
 
+    def initialize(errors)
+      @errors = errors
+    end
+
     def to_s
-      "Invalid configuration"
+      strings = ["Invalid configuration:"]
+
+      @errors.messages.each do |key, errors|
+        errors.each do |error|
+          strings << "  #{key} #{error}"
+        end
+      end
+
+      strings.join "\n"
     end
   end
 end
