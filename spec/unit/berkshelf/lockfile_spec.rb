0d5aafafa0ecd0d2a258673591fb236a033f6bc7
Jamie Winsor <jamie@vialstudios.com>
2012-11-11 14:19:11 -0800

    remove god awful lockfile spec - fixup lockfile in 1.1

--- spec/unit/berkshelf/lockfile_spec.rb
+++ spec/unit/berkshelf/lockfile_spec.rb
@@ -1,34 +1,5 @@
 require 'spec_helper'
 
-module Berkshelf
-  describe Lockfile do
-    let(:downloader) { Downloader.new(Berkshelf.cookbook_store) }
-
-    describe "without a lockfile in place already" do
-      before(:all) do
-        @old_dir = Dir.pwd
-        Dir.chdir fixtures_path.join("lockfile_spec", "without_lock")
-      end
-
-      after(:all) do
-        FileUtils.rm(fixtures_path.join("lockfile_spec", "without_lock", "Berksfile.lock"))
-        Dir.chdir(@old_dir)
-      end
-
-      it "should be able to write a Berksfile.lock from a list of cookbooks" do
-        resolver = Resolver.new(downloader, sources: CookbookSource.new('nginx', constraint: '= 0.101.0'))
-        resolver.resolve
-
-        Lockfile.new(resolver.sources).write
-
-        File.read('Berksfile.lock').split(/\r?\n/).should =~ [
-          "cookbook 'bluepill', :locked_version => '1.1.0'",
-          "cookbook 'build-essential', :locked_version => '1.1.2'",
-          "cookbook 'nginx', :locked_version => '0.101.0'",
-          "cookbook 'ohai', :locked_version => '1.0.2'",
-          "cookbook 'runit', :locked_version => '0.15.0'"
-        ]
-      end
-    end
-  end
+describe Berkshelf::Lockfile do
+  pending
 end
