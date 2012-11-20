8873c231dee473d542b03379ad0a45588417cf22
Justin Campbell <justin@justincampbell.me>
2012-11-20 08:29:57 -0500

    `berks open` raises on error, add features

--- features/step_definitions/utility_steps.rb
+++ features/step_definitions/utility_steps.rb
@@ -1,3 +1,11 @@
 Given /^pending\s+"([^\"]+)"$/ do |msg|
   pending
 end
+
+Given /^the environment variable (.+) is nil$/ do |variable|
+  set_env variable, nil
+end
+
+Given /^the environment variable (.+) is "(.+)"$/ do |variable, value|
+  set_env variable, value
+end
