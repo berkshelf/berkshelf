e37ba4182e3244af4396d2483b3481becf71a655
Jamie Winsor <jamie@vialstudios.com>
2013-02-05 14:29:47 -0800

    fix broken configure feature
    feature was broken when default values for the berkshelf configuration were inspecting
    a Chef Config file if it was present

---
+++ spec/support/test_generators.rb
@@ -0,0 +1,19 @@
+module Berkshelf
+  module TestGenerators
+    # Generate a minimal, default, Chef configuration file
+    #
+    # @param [#to_s] path
+    #   path to the configuration to generate
+    def generate_chef_config(path)
+      contents = <<-TXT
+chef_server_url "http://localhost:4000"
+validation_key "/etc/chef/validation.pem"
+validation_client_name "chef-validator"
+client_key "/etc/chef/client.pem"
+      TXT
+      File.open(path, 'w+') do |f|
+        f.write(contents)
+      end
+    end
+  end
+end
