75fefc0d43c65ec9d081066cbe51bc9e43c15c38
Jamie Winsor <jamie@vialstudios.com>
2013-02-05 19:14:45 -0800

    update lockfile spec to check for existence of file

--- spec/unit/berkshelf/lockfile_spec.rb
+++ spec/unit/berkshelf/lockfile_spec.rb
@@ -1,58 +1,58 @@
 require 'spec_helper'
 
 describe Berkshelf::Lockfile do
-  describe '.from_file' do
+  describe ".from_file" do
     let(:filename) { 'Berksfile.lock' }
     let(:lockfile) { Berkshelf::Lockfile.from_file(filename) }
     let(:content) { File.read(fixtures_path.join('lockfiles/default.lock')) }
+
+    subject { lockfile }
 
     before do
       content
       File.stub(:read).and_return(content)
     end
 
-    it 'has the correct sha' do
+    it "has the correct sha" do
       expect(lockfile.sha).to eq('6b76225554cc1f7c0aea0f8b3f10c6743aeba67e')
     end
 
-    it 'has the correct sources' do
+    it "has the correct sources" do
       expect(lockfile.sources.map(&:name)).to eq(['build-essential', 'chef-client'])
     end
 
-    context 'when the file does not exist' do
+    context "when the file does not exist" do
       before do
         ::File.stub(:read).and_raise(::Errno::ENOENT)
       end
 
-      it 'raises a Berkshelf::LockfileNotFound' do
+      it "raises a Berkshelf::LockfileNotFound" do
         expect {
           ::Berkshelf::Lockfile.from_file(filename)
         }.to raise_error(::Berkshelf::LockfileNotFound)
       end
     end
 
-    describe '#save' do
-      let(:file) { double('file') }
-
-      before do
-        ::File.stub(:open).and_yield(file)
+    describe "#save" do
+      before(:each) do
+        subject.stub(:filepath) { tmp_path.join('speclockfile.json')}
       end
 
-      it 'sends write out a new file' do
-        ::File.should_receive(:open).with(/Berksfile\.lock/, 'wb').and_yield(file)
-        file.should_receive(:write).with(content)
-        lockfile.save
+      it "saves itself to a file on disk" do
+        subject.save
+
+        File.exist?(subject.filepath).should be_true
       end
     end
 
-    describe '#update' do
-      it 'raises a Berkshelf::ArgumentError if one of the sources is not valid' do
+    describe "#update" do
+      it "raises a Berkshelf::ArgumentError if one of the sources is not valid" do
         expect {
           lockfile.update(['foo'])
         }.to raise_error(::Berkshelf::ArgumentError)
       end
 
-      it 'sets the @sources instance variable' do
+      it "sets the @sources instance variable" do
         source = double('source')
         source.stub(:is_a?).with(any_args()).and_return(true)
 
@@ -61,14 +61,14 @@ describe Berkshelf::Lockfile do
       end
     end
 
-    describe '#append' do
-      it 'raises a Berkshelf::ArgumentError if one of the sources is not valid' do
+    describe "#append" do
+      it "raises a Berkshelf::ArgumentError if one of the sources is not valid" do
         expect {
           lockfile.append('foo')
         }.to raise_error(::Berkshelf::ArgumentError)
       end
 
-      it 'adds the new source to the @sources instance variable' do
+      it "adds the new source to the @sources instance variable" do
         source = double(name: 'source')
         source.stub(:is_a?).with(any_args()).and_return(true)
 
@@ -76,7 +76,7 @@ describe Berkshelf::Lockfile do
         expect(lockfile.sources.map(&:name)).to eq(['build-essential', 'chef-client', 'source'])
       end
 
-      it 'does not add duplicate sources' do
+      it "does not add duplicate sources" do
         source = double(name: 'source')
         source.stub(:is_a?).with(any_args()).and_return(true)
 
@@ -88,20 +88,20 @@ describe Berkshelf::Lockfile do
       end
     end
 
-    describe '#to_hash' do
+    describe "#to_hash" do
       let(:hash) { lockfile.to_hash }
 
-      it 'has the :sha key' do
+      it "has the :sha key" do
         expect(hash).to have_key(:sha)
       end
 
-      it 'has the :sources key' do
+      it "has the :sources key" do
         expect(hash).to have_key(:sources)
       end
     end
 
-    describe '#to_json' do
-      it 'dumps the #to_hash to MultiJson' do
+    describe "#to_json" do
+      it "dumps the #to_hash to MultiJson" do
         MultiJson.should_receive(:dump).with(lockfile.to_hash, pretty: true)
         lockfile.to_json
       end
