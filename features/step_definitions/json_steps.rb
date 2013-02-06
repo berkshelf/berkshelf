a2c932cc915ffa2fd67c3c5762d589a820aaeec5
Jamie Winsor <jamie@vialstudios.com>
2013-02-06 13:28:27 -0800

    ignore whitespace and other annoyances when checking JSON in features

--- features/step_definitions/json_steps.rb
+++ features/step_definitions/json_steps.rb
@@ -1,3 +1,10 @@
 Then /^the output should be JSON$/ do
   lambda { parse_json(all_output) }.should_not raise_error
 end
+
+Then /^the file "(.*?)" should contain JSON:$/ do |file, data|
+  target = MultiJson.decode(data)
+  actual = MultiJson.decode(File.read(File.join(current_dir, file)))
+
+  target.should eql(actual)
+end
