589fbb08abe757904fc026c850bfd130278d072e
Fred Blasdel <blasdelf@gmail.com>
2013-02-07 11:20:09 -0800

    squashme

--- lib/berkshelf.rb
+++ lib/berkshelf.rb
@@ -73,7 +73,7 @@ module Berkshelf
     #
     # @return [String]
     def berkshelf_path
-      ENV["BERKSHELF_PATH"] || File.expand_path(DEFAULT_STORE_PATH)
+      @berkshelf_path ||= ENV["BERKSHELF_PATH"] || File.expand_path("~/.berkshelf")
     end
 
     # Check if we're running a version of Chef that is in the 11.x line
