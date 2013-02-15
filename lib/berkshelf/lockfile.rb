f52c047adaf3d1ece48ae9cb2e2d7c004e2ab9c0
Seth Vargo <sethvargo@gmail.com>
2013-02-15 17:17:07 -0500

    Use a hash instead of an array for sources

--- lib/berkshelf/lockfile.rb
+++ lib/berkshelf/lockfile.rb
@@ -33,7 +33,7 @@ module Berkshelf
 
         lockfile = self.new(sha: hash[:sha], filepath: filepath)
         hash[:sources].each do |name, options|
-          lockfile.add Berkshelf::CookbookSource.new(name, options)
+          lockfile.add Berkshelf::CookbookSource.new(name.to_s, options)
         end
         lockfile
       end
@@ -64,6 +64,12 @@ module Berkshelf
       @sources = {}
     end
 
+    # Set the sha value to nil to mark that the lockfile is not out of
+    # sync with the Berksfile.
+    def reset_sha!
+      @sha = nil
+    end
+
     # The list of sources constrained in this lockfile.
     #
     # @return [Array<Berkshelf::CookbookSource>]
@@ -72,33 +78,41 @@ module Berkshelf
       @sources.values
     end
 
-    # Check if this lockfile has the given source
+    # Find the given source in this lockfile. This method accepts a source
+    # attribute which may either be the name of a cookbook (String) or an
+    # actual cookbook source.
     #
-    # @param [#to_s] source
-    #   the source to check presence of
+    # @param [String, Berkshelf::CookbookSource] source
+    #   the cookbook source/name to find
+    # @return [CookbookSource, nil]
+    #   the cookbook source from this lockfile or nil if one was not found
+    def find(source)
+      @sources[cookbook_name(source).to_s]
+    end
+
+    # Determine if this lockfile contains the given source.
     #
+    # @param [String, Berkshelf::CookbookSource] source
+    #   the cookbook source/name to determine existence of
     # @return [Boolean]
     #   true if the source exists, false otherwise
     def has_source?(source)
-      @sources.has_key?(source.to_s)
+      !find(source).nil?
     end
-
-    # Save the contents of the lockfile to disk.
-    def save
-      File.open(filepath, 'w') do |file|
-        file.write self.to_json + "\n"
-      end
-    end
-    alias_method :write, :save
 
     # Replace the current list of sources with `sources`. This method does
     # not write out the lockfile - it only changes the state of the object.
     #
     # @param [Array<Berkshelf::CookbookSource>] sources
     #   the list of sources to update
-    def update(sources)
-      @sources = {}
+    # @option options [String] :sha
+    #   the sha of the Berksfile updating the sources
+    def update(sources, options = {})
+      reset_sources!
+      @sha = options[:sha]
+
       sources.each { |source| append(source) }
+      save
     end
 
     # Add the given source to the `sources` list, if it doesn't already exist.
@@ -106,9 +120,29 @@ module Berkshelf
     # @param [Berkshelf::CookbookSource] source
     #   the source to append to the sources list
     def add(source)
-      @sources[source.name] = source
+      @sources[cookbook_name(source)] = source
     end
     alias_method :append, :add
+
+    # Remove the given source from this lockfile. This method accepts a source
+    # attribute which may either be the name of a cookbook (String) or an
+    # actual cookbook source.
+    #
+    # @param [String, Berkshelf::CookbookSource] source
+    #   the cookbook source/name to remove
+    #
+    # @raise [Berkshelf::CookbookNotFound]
+    #   if the provided source does not exist
+    def remove(source)
+      unless has_source?(source)
+        raise Berkshelf::CookbookNotFound,
+          "'#{cookbook_name(source)}' does not exist in this lockfile!"
+      end
+
+      @sources.delete(cookbook_name(source))
+    end
+    alias_method :unlock, :remove
+
 
     # @return [String]
     #   the string representation of the lockfile
@@ -142,65 +176,89 @@ module Berkshelf
     # @return [String]
     #   the JSON representation of this lockfile
     def to_json
-      MultiJson.dump(self.to_hash, pretty: true)
+      MultiJson.dump(to_hash, pretty: true)
     end
-  end
 
-  private
+    private
 
-    # Legacy support for old lockfiles
-    #
-    # @author Seth Vargo <sethvargo@gmail.com>
-    # @todo Remove this class in the next major release.
-    class LockfileLegacy
-      class << self
-        # Read the old lockfile content and instance eval in context.
-        #
-        # @param [String] content
-        #   the string content read from a legacy lockfile
-        def parse(content)
-          sources = {}.tap do |hash|
-            content.split("\n").each do |line|
-              next if line.empty?
-
-              source = self.new(line)
-              hash[source.name] = source.options
-            end
-          end
-
-          {
-            sha: nil,
-            sources: sources
-          }
+      # Save the contents of the lockfile to disk.
+      def save
+        File.open(filepath, 'w') do |file|
+          file.write to_json + "\n"
         end
       end
 
-      # @return [Hash]
-      #   the hash of options
-      attr_reader :options
+      # Clear the sources array
+      def reset_sources!
+        @sources = {}
+      end
 
+      # Return the name of this cookbook (because it's the key in our
+      # table).
+      #
+      # @param [Berkshelf::CookbookSource, #to_s] source
+      #   the source to find the name from
+      #
       # @return [String]
-      #   the name of this cookbook
-      attr_reader :name
-
-      # Create a new legacy lockfile for processing
-      #
-      # @param [String] content
-      #   the content to parse out and convert to a hash
-      def initialize(content)
-        instance_eval(content).to_hash
+      #   the name of the cookbook
+      def cookbook_name(source)
+        source.is_a?(CookbookSource) ? source.name : source.to_s
       end
 
-      # Method defined in legacy lockfiles (since we are using
-      # instance_eval).
+      # Legacy support for old lockfiles
       #
-      # @param [String] name
-      #   the name of this cookbook
-      # @option options [String] :locked_version
-      #   the locked version of this cookbook
-      def cookbook(name, options = {})
-        @name = name
-        @options = options
+      # @author Seth Vargo <sethvargo@gmail.com>
+      # @todo Remove this class in the next major release.
+      class LockfileLegacy
+        class << self
+          # Read the old lockfile content and instance eval in context.
+          #
+          # @param [String] content
+          #   the string content read from a legacy lockfile
+          def parse(content)
+            sources = {}.tap do |hash|
+              content.split("\n").each do |line|
+                next if line.empty?
+
+                source = self.new(line)
+                hash[source.name] = source.options
+              end
+            end
+
+            {
+              sha: nil,
+              sources: sources
+            }
+          end
+        end
+
+        # @return [Hash]
+        #   the hash of options
+        attr_reader :options
+
+        # @return [String]
+        #   the name of this cookbook
+        attr_reader :name
+
+        # Create a new legacy lockfile for processing
+        #
+        # @param [String] content
+        #   the content to parse out and convert to a hash
+        def initialize(content)
+          instance_eval(content).to_hash
+        end
+
+        # Method defined in legacy lockfiles (since we are using
+        # instance_eval).
+        #
+        # @param [String] name
+        #   the name of this cookbook
+        # @option options [String] :locked_version
+        #   the locked version of this cookbook
+        def cookbook(name, options = {})
+          @name = name
+          @options = options
+        end
       end
-    end
+  end
 end
