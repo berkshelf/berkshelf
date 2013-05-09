bac7d525f6b80241afe785c9f8102a4a8d0fec45
Seth Vargo <sethvargo@gmail.com>
2013-05-09 18:27:32 -0400

    Use describe instead of context for methods on logger_spec

--- spec/unit/berkshelf/logger_spec.rb
+++ spec/unit/berkshelf/logger_spec.rb
@@ -2,42 +2,42 @@ require 'spec_helper'
 
 describe Berkshelf::Logger do
   # Berkshelf::Logger#info
-  context '#info' do
+  describe '#info' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:info)
     end
   end
 
   # Berkshelf::Logger#warn
-  context '#warn' do
+  describe '#warn' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:warn)
     end
   end
 
   # Berkshelf::Logger#error
-  context '#error' do
+  describe '#error' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:error)
     end
   end
 
   # Berkshelf::Logger#fatal
-  context '#fatal' do
+  describe '#fatal' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:fatal)
     end
   end
 
   # Berkshelf::Logger#debug
-  context '#debug' do
+  describe '#debug' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:debug)
     end
   end
 
   # Berkshelf::Logger#deprecate
-  context '#deprecate' do
+  describe '#deprecate' do
     it 'responds' do
       expect(Berkshelf::Logger).to respond_to(:deprecate)
     end
