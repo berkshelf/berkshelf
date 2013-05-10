1068d74dcf0ebb55408b8a3932f500b16940a86b
Seth Vargo <sethvargo@gmail.com>
2013-05-09 22:22:38 -0400

    Refactor unit/berkshelf/config_spec

--- spec/unit/berkshelf/config_spec.rb
+++ spec/unit/berkshelf/config_spec.rb
@@ -1,39 +1,37 @@
 require 'spec_helper'
 
 describe Berkshelf::Config do
-  let(:klass) { described_class }
+  #
+  # Class Methods
+  # -------------------------
 
-  describe "ClassMethods" do
-    subject { klass }
+  # Berkshelf::Config.file
+  describe '.file' do
+    context 'when the file does not exist' do
+      before { File.stub(:exists?).and_return(false) }
 
-    describe "::file" do
-      subject { klass.file }
-
-      context "when the file does not exist" do
-        before :each do
-          File.stub exists?: false
-        end
-
-        it { should be_nil }
+      it 'is nil' do
+        expect(Berkshelf::Config.file).to be_nil
       end
     end
+  end
 
-    describe "::instance" do
-      subject { klass.instance }
+  # Berkshelf::Config.instance
+  describe '.instance' do
+    it 'should be a Berkshelf::Config' do
+      expect(Berkshelf::Config.instance).to be_an_instance_of(Berkshelf::Config)
+    end
+  end
 
-      it { should be_a klass }
+  # Berkshelf::Config.path
+  describe '.path' do
+    it 'is a string' do
+      expect(Berkshelf::Config.path).to be_a(String)
     end
 
-    describe "::path" do
-      subject { klass.path }
-
-      it { should be_a String }
-
-      it "points to a location within ENV['BERKSHELF_PATH']" do
-        ENV.stub(:[]).with('BERKSHELF_PATH').and_return('/tmp')
-
-        subject.should eql("/tmp/config.json")
-      end
+    it "points to a location within ENV['BERKSHELF_PATH']" do
+      ENV.stub(:[]).with('BERKSHELF_PATH').and_return('/tmp')
+      expect(Berkshelf::Config.path).to eq('/tmp/config.json')
     end
   end
 end
