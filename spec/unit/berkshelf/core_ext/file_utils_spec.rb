8e8409ebac1d66e170ccdfd2d64f5a7dd2a5f0b2
Seth Vargo <sethvargo@gmail.com>
2013-05-09 23:06:35 -0400

    Refactor unit/berkshelf/core_ext/file_utils_spec

--- spec/unit/berkshelf/core_ext/file_utils_spec.rb
+++ spec/unit/berkshelf/core_ext/file_utils_spec.rb
@@ -1,12 +1,13 @@
 require 'spec_helper'
 
 describe FileUtils do
-  describe "#mv" do
+  # FileUtils#mv
+  describe '#mv' do
     let(:src) { double('src') }
     let(:dest) { double('dest') }
     let(:options) { double('options') }
 
-    it "replaces mv with cp_r and rm_rf" do
+    it 'replaces mv with cp_r and rm_rf' do
       subject.stub(:windows?) { true }
       FileUtils.should_receive(:cp_r).with(src, dest, options)
       FileUtils.should_receive(:rm_rf).with(src)
