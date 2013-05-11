fb1448849c58e96440df13c7f43d42d4a8bb5105
Seth Vargo <sethvargo@gmail.com>
2013-05-10 21:53:01 -0400

    Remove comments on ConfigSpec

--- spec/unit/berkshelf/config_spec.rb
+++ spec/unit/berkshelf/config_spec.rb
@@ -1,11 +1,6 @@
 require 'spec_helper'
 
 describe Berkshelf::Config do
-  #
-  # Class Methods
-  # -------------------------
-
-  # Berkshelf::Config.file
   describe '.file' do
     context 'when the file does not exist' do
       before { File.stub(:exists?).and_return(false) }
@@ -16,14 +11,12 @@ describe Berkshelf::Config do
     end
   end
 
-  # Berkshelf::Config.instance
   describe '.instance' do
     it 'should be a Berkshelf::Config' do
       expect(Berkshelf::Config.instance).to be_an_instance_of(Berkshelf::Config)
     end
   end
 
-  # Berkshelf::Config.path
   describe '.path' do
     it 'is a string' do
       expect(Berkshelf::Config.path).to be_a(String)
