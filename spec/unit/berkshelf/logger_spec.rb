b0fec782234845b4f8a3e9f1bffc567a25d86d9b
Seth Vargo <sethvargo@gmail.com>
2013-05-10 21:53:02 -0400

    Remove comments on LoggerSpec

--- spec/unit/berkshelf/logger_spec.rb
+++ spec/unit/berkshelf/logger_spec.rb
@@ -1,45 +1,11 @@
 require 'spec_helper'
 
 describe Berkshelf::Logger do
-  # Berkshelf::Logger#info
-  describe '#info' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:info)
-    end
-  end
-
-  # Berkshelf::Logger#warn
-  describe '#warn' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:warn)
-    end
-  end
-
-  # Berkshelf::Logger#error
-  describe '#error' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:error)
-    end
-  end
-
-  # Berkshelf::Logger#fatal
-  describe '#fatal' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:fatal)
-    end
-  end
-
-  # Berkshelf::Logger#debug
-  describe '#debug' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:debug)
-    end
-  end
-
-  # Berkshelf::Logger#deprecate
-  describe '#deprecate' do
-    it 'responds' do
-      expect(Berkshelf::Logger).to respond_to(:deprecate)
+  %w(info warn error fatal debug deprecate).each do |meth|
+    describe "##{meth}" do
+      it 'responds' do
+        expect(Berkshelf::Logger).to respond_to(meth.to_sym)
+      end
     end
   end
 end
