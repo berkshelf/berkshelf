30589bafb1e3a6150bcaffb092c224a65ecdac96
Seth Vargo <sethvargo@gmail.com>
2013-05-14 21:35:05 -0400

    Fix Null Formatter. Add specs

--- lib/berkshelf/formatters/null.rb
+++ lib/berkshelf/formatters/null.rb
@@ -6,6 +6,15 @@ module Berkshelf
 
       register_formatter :null
 
+      # The abstract formatter dynamically defines methods that raise an
+      # AbstractFunction error. We need to define all of those on our class,
+      # otherwise they will be inherited by the Ruby object model.
+      AbstractFormatter.instance_methods.each do |meth|
+        define_method(meth) do |*args|
+          # intentionally do nothing
+        end
+      end
+
       def method_missing(meth, *args, &block)
         # intentionally do nothing
       end
