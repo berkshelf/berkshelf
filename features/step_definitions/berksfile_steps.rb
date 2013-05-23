7869acaf4aba3d934e31596a8797ae175d015ee3
Seth Vargo <sethvargo@gmail.com>
2013-05-23 01:31:17 -0500

    Remove test generators

--- features/step_definitions/berksfile_steps.rb
+++ features/step_definitions/berksfile_steps.rb
@@ -1,10 +1,10 @@
 Given /^a Berksfile with path location sources to fixtures:$/ do |cookbooks|
   lines = []
   cookbooks.raw.each do |name, fixture|
-    fixture_path = fixtures_path.join("cookbooks", fixture)
+    fixture_path = File.join(fixtures_path, 'cookbooks', fixture)
     lines << "cookbook '#{name}', path: '#{fixture_path}'"
   end
-  write_file("Berksfile", lines.join("\n"))
+  write_file('Berksfile', lines.join("\n"))
 end
 
 Given /^I do not have a Berksfile$/ do
--- features/step_definitions/chef_config_steps.rb
+++ features/step_definitions/chef_config_steps.rb
@@ -1,7 +1,18 @@
-Given /^I have a default Chef config$/ do
-  STDOUT.puts "Standard Chef Config"
-  path = tmp_path.join('knife.rb').to_s
-  generate_chef_config(path)
+Given /^I have the default Chef config$/ do
+  path = File.join('spec', 'config', 'knife.rb').to_s
+  contents = [
+    'chef_server_url          "http://localhost:4000"',
+    'validation_key           "/etc/chef/validation.pem"',
+    'validation_client_name   "chef-validator"',
+    'client_key               "/etc/chef/client.pem"'
+  ].join("\n")
+
+  File.open(path, 'w+') { |f| f.write(contents) }
 
   Berkshelf::Chef::Config.from_file(path)
+end
+
+Given /^I do not have a Chef config$/ do
+  remove_file(Berkshelf::Chef::Config.path) if File.exists?(Berkshelf::Chef::Config.path)
+  Berkshelf::Chef::Config.instance_variable_set(:@instance, nil)
 end
