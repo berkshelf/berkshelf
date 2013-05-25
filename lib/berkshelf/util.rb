5a2f94edd170476aa4b707294e3c83437f491a4e
Seth Vargo <sethvargo@gmail.com>
2013-05-25 16:09:41 -0400

    Fix shell_out on JRuby

--- lib/berkshelf/util.rb
+++ lib/berkshelf/util.rb
@@ -6,8 +6,10 @@ module Berkshelf
     # @author Jamie Winsor <reset@riotgames.com>
     class << self
       # Perform a cross-platform shell command, returning the process result.
-      # This uses Process.spawn under the hood, so see {Process.spawn} for more
-      # information.
+      # This uses Process.spawn under the hood for Ruby 1.9, so see
+      # {Process.spawn} for more information.
+      #
+      # On JRuby, a system command is used and $stdout and $stderr are captured.
       #
       # @param [String] command
       # @param [Hash] options
@@ -20,21 +22,50 @@ module Berkshelf
       #     - pid
       #     - success?
       #     - failure?
-      def shell_out(command, options = {})
-        @stdout, @stderr = Tempfile.new('berkshelf.stdout'), Tempfile.new('berkshelf.stderr')
-        options = { out: @stdout.to_i, err: @stderr.to_i }.merge(options)
+      def shell_out(command)
+        if defined?(RUBY_PLATFORM) && RUBY_PLATFORM == 'java' # jruby
+          out, err, e = capture do
+            system(command)
+          end
+        else
+          begin
+            out_file, err_file = Tempfile.new('berkshelf.stdout'), Tempfile.new('berkshelf.stderr')
+            pid = Process.spawn({}, command, out: out_file.to_i, err: err_file.to_i)
+            Process.waitpid(pid)
 
-        pid = Process.spawn(command, options)
-        Process.waitpid(pid)
+            out, err = File.read(out_file), File.read(err_file)
+            e = $?
+          rescue Errno::ENOENT
+            out, err = "", "command not found: #{command}"
+            e = $?
+          end
+        end
 
         Hashie::Mash.new({
-          stdout:         File.read(@stdout).strip,
-          stderr:         File.read(@stderr).strip,
-          exitstatus:     $?.exitstatus,
-          pid:            $?.pid,
-          success?:       $?.success?,
-          failure?:       !$?.success?
+          stdout:         out.strip,
+          stderr:         err.strip,
+          exitstatus:     e.exitstatus,
+          pid:            e.pid,
+          success?:       e.success?,
+          failure?:       !e.success?
         })
+      end
+
+      # Execute the given block, capturing $stdout, $stderr, and the returned process.
+      #
+      # @return [Array<StringIO, StringIO, Process>]
+      #   a tuple of $stdout, $stderr, and the Process
+      def capture(&block)
+        out, err = StringIO.new, StringIO.new
+        $stdout, $stderr = out, err
+
+        yield
+
+        out.rewind
+        err.rewind
+        return out.read, err.read, $?
+      ensure
+        $stdout, $stderr = STDOUT, STDERR
       end
 
       # Retry the given block
