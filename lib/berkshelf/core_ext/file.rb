25ae71cc7484c44053e2c5654e916b1ba45d7c52
Seth Vargo <sethvargo@gmail.com>
2013-05-27 11:04:43 -0500

    Use single quotes in file.rb

--- lib/berkshelf/core_ext/file.rb
+++ lib/berkshelf/core_ext/file.rb
@@ -7,7 +7,7 @@ class File
     #
     # @return [Boolean]
     def cookbook?(path)
-      File.exists?(File.join(path, "metadata.rb"))
+      File.exists?(File.join(path, 'metadata.rb'))
     end
     alias_method :chef_cookbook?, :cookbook?
   end
