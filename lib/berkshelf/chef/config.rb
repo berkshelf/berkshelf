6c3c6a824d7922290e845bead15f3eb14afd8ecf
Seth Vargo <sethvargo@gmail.com>
2013-05-27 17:04:56 -0700

    Move Eigenclass back to the top

--- lib/berkshelf/chef/config.rb
+++ lib/berkshelf/chef/config.rb
@@ -12,27 +12,6 @@ module Berkshelf
       include Berkshelf::Mixin::Config
       extend Berkshelf::Mixin::PathHelpers
 
-      default_option(:node_name,                Socket.gethostname)
-      default_option(:chef_server_url,          'http://localhost:4000')
-      default_option(:client_key,               platform_specific_path('/etc/chef/client.pem'))
-      default_option(:validation_key,           platform_specific_path('/etc/chef/validation.pem'))
-      default_option(:validation_client_name,   'chef-validator')
-
-      default_option(:cookbook_copyright,       'YOUR_NAME')
-      default_option(:cookbook_email,           'YOUR_EMAIL')
-      default_option(:cookbook_license,         'reserved')
-
-      default_option(:knife, {})
-
-      # Prior to Chef 11, the cache implementation was based on
-      # moneta and configured via cache_options[:path]. Knife configs
-      # generated with Chef 11 will have `syntax_check_cache_path`, but older
-      # configs will have `cache_options[:path]`. `cache_options` is marked
-      # deprecated in chef/config.rb but doesn't currently trigger a warning.
-      # See also: CHEF-3715
-      default_option(:syntax_check_cache_path,  Dir.mktmpdir)
-      default_option(:cache_options,            { path: defined?(syntax_check_cache_path) ? syntax_check_cache_path : Dir.mktmpdir })
-
       class << self
         private
           # Return the most sensible path to the Chef configuration file. This can
@@ -68,6 +47,27 @@ module Berkshelf
             ENV['PWD'] || Dir.pwd
           end
       end
+
+      default_option(:node_name,                Socket.gethostname)
+      default_option(:chef_server_url,          'http://localhost:4000')
+      default_option(:client_key,               platform_specific_path('/etc/chef/client.pem'))
+      default_option(:validation_key,           platform_specific_path('/etc/chef/validation.pem'))
+      default_option(:validation_client_name,   'chef-validator')
+
+      default_option(:cookbook_copyright,       'YOUR_NAME')
+      default_option(:cookbook_email,           'YOUR_EMAIL')
+      default_option(:cookbook_license,         'reserved')
+
+      default_option(:knife, {})
+
+      # Prior to Chef 11, the cache implementation was based on
+      # moneta and configured via cache_options[:path]. Knife configs
+      # generated with Chef 11 will have `syntax_check_cache_path`, but older
+      # configs will have `cache_options[:path]`. `cache_options` is marked
+      # deprecated in chef/config.rb but doesn't currently trigger a warning.
+      # See also: CHEF-3715
+      default_option(:syntax_check_cache_path,  Dir.mktmpdir)
+      default_option(:cache_options,            { path: defined?(syntax_check_cache_path) ? syntax_check_cache_path : Dir.mktmpdir })
     end
   end
 end
