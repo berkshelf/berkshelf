d840c021549f6f6b69250f2f10286fb907defe02
Seth Vargo <sethvargo@gmail.com>
2013-05-29 20:44:31 -0700

    Single quotes

--- lib/berkshelf/chef/config.rb
+++ lib/berkshelf/chef/config.rb
@@ -67,10 +67,10 @@ module Berkshelf::Chef
     extend Mixlib::Config
 
     node_name               Socket.gethostname
-    chef_server_url         "http://localhost:4000"
-    client_key              Berkshelf::Util.platform_specific_path("/etc/chef/client.pem")
-    validation_key          Berkshelf::Util.platform_specific_path("/etc/chef/validation.pem")
-    validation_client_name  "chef-validator"
+    chef_server_url         'http://localhost:4000'
+    client_key              Berkshelf::Util.platform_specific_path('/etc/chef/client.pem')
+    validation_key          Berkshelf::Util.platform_specific_path('/etc/chef/validation.pem')
+    validation_client_name  'chef-validator'
 
     cookbook_copyright      'YOUR_NAME'
     cookbook_email          'YOUR_EMAIL'
