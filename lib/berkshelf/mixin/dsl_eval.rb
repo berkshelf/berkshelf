5df497e35cd12ce98736232ed9fa02dd43c833e0
Jamie Winsor <jamie@vialstudios.com>
2013-06-10 15:30:48 -0700

    only expose methods we want to the Berksfile dsl

---
+++ lib/berkshelf/mixin/dsl_eval.rb
@@ -0,0 +1,46 @@
+module Berkshelf
+  module Mixin
+    module DSLEval
+      class CleanRoom
+        attr_reader :instance
+
+        def initialize(instance)
+          @instance = instance
+        end
+      end
+
+      class << self
+        def included(base)
+          base.send(:extend, ClassMethods)
+        end
+      end
+
+      module ClassMethods
+        def clean_room
+          exposed_methods = self.exposed_methods
+
+          @clean_room ||= Class.new(DSLEval::CleanRoom) do
+            exposed_methods.each do |exposed_method|
+              define_method(exposed_method) do |*args|
+                instance.send(exposed_method, *args)
+              end
+            end
+          end
+        end
+
+        def expose_method(method)
+          exposed_methods << method.to_sym
+        end
+
+        def exposed_methods
+          @exposed_methods ||= Array.new
+        end
+      end
+
+      def dsl_eval(&block)
+        self.class.clean_room.new(self).instance_eval(&block)
+        self
+      end
+    end
+  end
+end
