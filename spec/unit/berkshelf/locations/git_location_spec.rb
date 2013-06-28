1242fa2b83d4e301166a9d0057dcbb02238a4690
Jamie Winsor <jamie@vialstudios.com>
2013-06-27 20:02:38 -0700

    fix git location and path location specs

--- spec/unit/berkshelf/locations/git_location_spec.rb
+++ spec/unit/berkshelf/locations/git_location_spec.rb
@@ -1,25 +1,26 @@
 require 'spec_helper'
 
 describe Berkshelf::GitLocation do
-  let(:complacent_constraint) { double('comp-vconstraint', satisfies?: true) }
+  let(:constraint) { double('comp-vconstraint', satisfies?: true) }
+  let(:dependency) { double('dep', name: "berkshelf-cookbook-fixture", version_constraint: constraint) }
 
-  describe '.initialize' do
-    it 'raises InvalidGitURI if given an invalid Git URI for options[:git]' do
-      expect {
-        Berkshelf::GitLocation.new('berkshelf-cookbook-fixture', complacent_constraint, git: '/something/on/disk')
-      }.to raise_error(Berkshelf::InvalidGitURI)
+  describe "ClassMethods" do
+    describe "::new" do
+      it 'raises InvalidGitURI if given an invalid Git URI for options[:git]' do
+        expect {
+          described_class.new(dependency, git: '/something/on/disk')
+        }.to raise_error(Berkshelf::InvalidGitURI)
+      end
+    end
+
+    describe "::tmpdir" do
+      it 'creates a temporary directory within the Berkshelf temporary directory' do
+        expect(described_class.tmpdir).to include(Berkshelf.tmp_dir)
+      end
     end
   end
 
-  describe '.tmpdir' do
-    it 'creates a temporary directory within the Berkshelf temporary directory' do
-      expect(Berkshelf::GitLocation.tmpdir).to include(Berkshelf.tmp_dir)
-    end
-  end
-
-
-
-  subject { Berkshelf::GitLocation.new('berkshelf-cookbook-fixture', complacent_constraint, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git') }
+  subject { described_class.new(dependency, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git') }
 
   describe '#download' do
     context 'when a local revision is present' do
@@ -27,8 +28,8 @@ describe Berkshelf::GitLocation do
 
       before do
         Berkshelf::Git.stub(:rev_parse).and_return('abcd1234')
-        Berkshelf::GitLocation.any_instance.stub(:cached?).and_return(true)
-        Berkshelf::GitLocation.any_instance.stub(:validate_cached).with(cached).and_return(cached)
+        described_class.any_instance.stub(:cached?).and_return(true)
+        described_class.any_instance.stub(:validate_cached).with(cached).and_return(cached)
         Berkshelf::CachedCookbook.stub(:from_store_path).with(any_args()).and_return(cached)
       end
 
@@ -53,7 +54,7 @@ describe Berkshelf::GitLocation do
     end
 
     context 'given no ref/branch/tag options is given' do
-      subject { Berkshelf::GitLocation.new('berkshelf-cookbook-fixture', complacent_constraint, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git') }
+      subject { described_class.new(dependency, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git') }
 
       it 'sets the branch attribute to the HEAD revision of the cloned repo' do
         subject.download(tmp_path)
@@ -62,7 +63,8 @@ describe Berkshelf::GitLocation do
     end
 
     context 'given a git repo that does not exist' do
-      subject { Berkshelf::GitLocation.new('doesnot_exist', complacent_constraint, git: 'git://github.com/RiotGames/thisrepo_does_not_exist.git') }
+      before { dependency.stub(name: "doesnot_exist") }
+      subject { described_class.new(dependency, git: 'git://github.com/RiotGames/thisrepo_does_not_exist.git') }
 
       it 'raises a GitError' do
         Berkshelf::Git.stub(:git).and_raise(Berkshelf::GitError.new(''))
@@ -74,7 +76,8 @@ describe Berkshelf::GitLocation do
 
     context 'given a git repo that does not contain a cookbook' do
       let(:fake_remote) { remote_path('not_a_cookbook') }
-      subject { Berkshelf::GitLocation.new('doesnot_exist', complacent_constraint, git: "file://#{fake_remote}.git") }
+      before { dependency.stub(name: "doesnot_exist") }
+      subject { described_class.new(dependency, git: "file://#{fake_remote}.git") }
 
       it 'raises a CookbookNotFound error' do
         subject.stub(:clone).and_return {
@@ -89,17 +92,11 @@ describe Berkshelf::GitLocation do
     end
 
     context 'given the content at the Git repo does not satisfy the version constraint' do
-      subject do
-        Berkshelf::GitLocation.new('berkshelf-cookbook-fixture',
-          double('constraint', satisfies?: false),
-          git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git'
-        )
-      end
+      before { constraint.stub(satisfies?: false) }
+      subject { described_class.new(dependency, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git') }
 
       it 'raises a CookbookValidationFailure error' do
-        expect {
-          subject.download(tmp_path)
-        }.to raise_error(Berkshelf::CookbookValidationFailure)
+        expect { subject.download(tmp_path) }.to raise_error(Berkshelf::CookbookValidationFailure)
       end
     end
 
@@ -107,12 +104,9 @@ describe Berkshelf::GitLocation do
       let(:tag) { '1.0.0' }
 
       subject do
-        Berkshelf::GitLocation.new('berkshelf-cookbook-fixture',
-          complacent_constraint,
-          git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git',
-          tag: tag
-        )
+        described_class.new(dependency, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git', tag: tag)
       end
+
       let(:cached) { subject.download(tmp_path) }
       let(:sha) { subject.ref }
       let(:expected_path) { tmp_path.join("#{cached.cookbook_name}-#{sha}") }
@@ -126,11 +120,8 @@ describe Berkshelf::GitLocation do
       let(:branch) { 'master' }
 
       subject do
-        Berkshelf::GitLocation.new('berkshelf-cookbook-fixture',
-          complacent_constraint,
-          git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git',
-          branch: branch
-        )
+        described_class.new(dependency, git: 'git://github.com/RiotGames/berkshelf-cookbook-fixture.git',
+          branch: branch)
       end
       let(:cached) { subject.download(tmp_path) }
       let(:sha) { subject.ref }
--- spec/unit/berkshelf/locations/path_location_spec.rb
+++ spec/unit/berkshelf/locations/path_location_spec.rb
@@ -1,32 +1,15 @@
 require 'spec_helper'
 
 describe Berkshelf::PathLocation do
-  let(:complacent_constraint) { double('comp-vconstraint', satisfies?: true) }
+  let(:constraint) { double('comp-vconstraint', satisfies?: true) }
+  let(:dependency) { double('dep', name: "nginx", version_constraint: constraint) }
   let(:path) { fixtures_path.join('cookbooks', 'example_cookbook').to_s }
 
-  describe '.new' do
-    it 'assigns the value of :path to @path' do
-      location = Berkshelf::PathLocation.new('nginx', complacent_constraint, path: path)
-      expect(location.path).to eq(path)
-    end
-  end
-
-  subject { Berkshelf::PathLocation.new('nginx', complacent_constraint, path: path) }
-
-  describe '#to_s' do
-    context 'for a remote path' do
-      subject { Berkshelf::PathLocation.new('nginx', complacent_constraint, path: path) }
-
-      it 'includes the path information' do
-        expect(subject.to_s).to match(/path\:.+example_cookbook/)
-      end
-    end
-
-    context 'for a store path' do
-      subject { Berkshelf::PathLocation.new('nginx', complacent_constraint, path: File.join(Berkshelf.berkshelf_path, 'cookbooks/example_cookbook')) }
-
-      it 'does not include the path information' do
-        expect(subject.to_s).to_not match(/path\:.+/)
+  describe "ClassMethods" do
+    describe "::new" do
+      it 'assigns the value of :path to @path' do
+        location = described_class.new(dependency, path: path)
+        expect(location.path).to eq(path)
       end
     end
   end
