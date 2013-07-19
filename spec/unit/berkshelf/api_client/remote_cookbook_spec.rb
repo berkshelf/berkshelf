3fad55daa89764c5c23f71e93e54e15364e32ef8
Jay Prall <jay.prall@affectiva.com>
2013-07-19 14:27:58 -0400

    clean hard tabs

--- spec/unit/berkshelf/api_client/remote_cookbook_spec.rb
+++ spec/unit/berkshelf/api_client/remote_cookbook_spec.rb
@@ -1,15 +1,15 @@
 require 'spec_helper'
 
 describe Berkshelf::APIClient::RemoteCookbook do
-	let(:name) { "ruby" }
-	let(:version) { "1.2.3" }
+  let(:name) { "ruby" }
+  let(:version) { "1.2.3" }
   let(:dependencies) { double('dependencies') }
   let(:platforms) { double('platforms') }
   let(:location_type) { "chef_server" }
   let(:location_path) { "http://localhost:8080" }
 
   let(:attributes) do
-  	{ dependencies: dependencies, platforms: platforms, location_path: location_path, location_type: location_type }
+    { dependencies: dependencies, platforms: platforms, location_path: location_path, location_type: location_type }
   end
 
   subject { described_class.new(name, version, attributes) }
