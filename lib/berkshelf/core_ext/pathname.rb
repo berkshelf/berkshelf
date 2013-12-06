f92821fc54b1509801f83dc30df18a310b4cd836
Jamie Winsor <jamie@vialstudios.com>
2013-12-06 12:41:27 -0800

    properly identify a cookbook on disk by it's metadata

--- lib/berkshelf/core_ext/pathname.rb
+++ lib/berkshelf/core_ext/pathname.rb
@@ -1,20 +1,18 @@
 class Pathname
-  # Returns true or false if the path of the instantiated
-  # Pathname or a parent directory contains a Tryhard Pack
-  # data directory.
+  # Returns true or false if the path contains a "metadata.json" or a "metadata.rb" file.
   #
   # @return [Boolean]
   def cookbook?
-    self.join('metadata.rb').exist?
+    join("metadata.json").exist? || join("metadata.rb").exist?
   end
   alias_method :chef_cookbook?, :cookbook?
 
-  # Ascend the directory structure from the given path to find a
-  # the root of a Chef Cookbook. If no Cookbook is found, nil is returned
+  # Ascend the directory structure from the given path to find  the root of a
+  # Cookbook. If no Cookbook is found, nil is returned.
   #
   # @return [Pathname, nil]
   def cookbook_root
-    self.ascend do |potential_root|
+    ascend do |potential_root|
       if potential_root.cookbook?
         return potential_root
       end
