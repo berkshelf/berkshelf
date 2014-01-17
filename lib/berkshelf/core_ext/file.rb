e6dc44203189b1dc73be2d05e21a3401f9218b5d
Jamie Winsor <jamie@vialstudios.com>
2014-01-17 11:40:47 -0800

    File.cookbook? will now properly look for raw and compiled metadata

--- lib/berkshelf/core_ext/file.rb
+++ lib/berkshelf/core_ext/file.rb
@@ -1,13 +1,13 @@
 class File
   class << self
-    # Returns true or false if the given path is a Chef Cookbook
+    # Returns true or false if the given path contains a Chef Cookbook
     #
     # @param [#to_s] path
     #   path of directory to reflect on
     #
     # @return [Boolean]
     def cookbook?(path)
-      File.exists?(File.join(path, 'metadata.rb'))
+      File.exists?(File.join(path, "metadata.json")) || File.exists?(File.join(path, "metadata.rb"))
     end
     alias_method :chef_cookbook?, :cookbook?
   end
