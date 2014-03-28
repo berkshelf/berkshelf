88c22d4bb7d322b89e61d55b9a5261fe8391ab57
Seth Vargo <sethvargo@gmail.com>
2014-03-27 22:26:12 -0400

    Fix logging
    The previous implementation of the logger was never actually setting
    Berkshelf's logger when you said +Berkshelf.logger = x+. This commit
    makes the Berkshelf::Logger inherit from Ridley's logger and actually
    works now :).

--- lib/berkshelf/logger.rb
+++ lib/berkshelf/logger.rb
@@ -1,12 +1,18 @@
 module Berkshelf
-  Logger = Ridley.logger
-
-  Logger.class.class_eval do
+  class Logger < Ridley::Logging::Logger
     alias_method :fatal, :error
 
     def deprecate(message)
       trace = caller.join("\n\t")
       warn "DEPRECATION WARNING: #{message}\n\t#{trace}"
+    end
+
+    # Log an exception and its backtrace to FATAL
+    #
+    # @param [Exception] ex
+    def exception(ex)
+      log.fatal("#{ex.class}: #{ex}")
+      log.fatal(ex.backtrace.join("\n")) unless ex.backtrace.nil?
     end
   end
 end
