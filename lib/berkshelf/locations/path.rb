a02cb357d7cee10e4c4e063c5a0d4ae580bf4dad
Seth Vargo <sethvargo@gmail.com>
2014-03-29 00:32:47 -0400

    Make PathLocation#relative_path be the path relative to the Berksfile

--- lib/berkshelf/locations/path.rb
+++ lib/berkshelf/locations/path.rb
@@ -21,16 +21,15 @@ module Berkshelf
       !!options[:metadata]
     end
 
-    # Return this PathLocation's path relative to the given target.
-    #
-    # @param [#to_s] target
-    #   the path to a file or directory to be relative to
+    # Return this PathLocation's path relative to the associated Berksfile. It
+    # is actually the path reative to the associated Berksfile's parent
+    # directory.
     #
     # @return [String]
     #   the relative path relative to the target
-    def relative_path(target = '.')
+    def relative_path
       my_path     = Pathname.new(path).expand_path
-      target_path = Pathname.new(target.to_s).expand_path
+      target_path = Pathname.new(dependency.berksfile.filepath).expand_path
       target_path = target_path.dirname if target_path.file?
 
       new_path = my_path.relative_path_from(target_path).to_s
@@ -50,17 +49,21 @@ module Berkshelf
     def ==(other)
       other.is_a?(PathLocation) &&
       other.metadata? == metadata? &&
-      other.expanded_path == expanded_path
+      other.relative_path == relative_path
     end
 
     def to_lock
-      out =  "    path: #{relative_path(dependency.berksfile.filepath)}\n"
+      out =  "    path: #{relative_path}\n"
       out << "    metadata: true\n" if metadata?
       out
     end
 
     def to_s
-      "source at #{relative_path(dependency.berksfile.filepath)}"
+      "source at #{relative_path}"
+    end
+
+    def inspect
+      "#<Berkshelf::PathLocation metadata: #{metadata?}, path: #{relative_path}>"
     end
   end
 end
