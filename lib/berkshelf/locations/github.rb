d0a5000586471758d84a5ca2c20bd20cad2968b2
Joe Stump <joe@joestump.net>
2014-05-07 14:58:15 -0700

    Added specs for GithubLocation and fixed a regression when passing :ssh as the protocol for github locations.

--- lib/berkshelf/locations/github.rb
+++ lib/berkshelf/locations/github.rb
@@ -1,7 +1,12 @@
 module Berkshelf
   class GithubLocation < GitLocation
     def initialize(dependency, options = {})
-      options[:git] = "git://github.com/#{options.delete(:github)}.git"
+      if options[:protocol] == :ssh
+        uri = 'git@github.com:%{github}.git'
+      else
+        uri = 'git://github.com/%{github}.git'
+      end
+      options[:git] = uri % {:github => options.delete(:github)}
       super
     end
   end
