029592da4b1aa1975faa2d562c324f218d71b7b2
Brandon W Maister <bwm@knewton.com>
2014-08-20 15:00:49 -0400

    Add a test that actually builds a viz graph
    On some levels this is an integration test, but it's pretty fast and
    it's configured to just not run if dot is not installed, so it should be
    fine for most people.

--- spec/unit/berkshelf/visualizer_spec.rb
+++ spec/unit/berkshelf/visualizer_spec.rb
@@ -1,3 +1,4 @@
+require 'rspec'
 require 'spec_helper'
 
 module Berkshelf
@@ -23,6 +24,14 @@ module Berkshelf
 
         it 'raises a GraphvizCommandFailed exception' do
           expect { subject.to_png }.to raise_error(GraphvizCommandFailed)
+        end
+      end
+
+      context 'when graphviz is installed', :requires_dot => true do
+        it 'builds a png' do
+          outfile = tmp_path.join('test-graph.png').to_s
+          lockfile = Lockfile.from_file fixtures_path.join('lockfiles/default.lock').to_s
+          Visualizer.from_lockfile(lockfile).to_png(outfile)
         end
       end
     end
