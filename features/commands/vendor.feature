berkshelf/berkshelf

commit 1bfc97d64d38443e3326e85bd002c48399739c34

first pass at new vendor command
Add Berkshelf::Berksfile#vendor
Add Berkshelf::Cli#vendor
Update formatters for vendor output

lib/berkshelf/berksfile.rb
@@ -522,6 +522,46 @@ module Berkshelf
       output
     end
 
+    # @param [String] destination
+    #
+    # @option options [Symbol, Array] :except
+    #   Group(s) to exclude which will cause any dependencies marked as a member of the
+    #   group to not be installed
+    # @option options [Symbol, Array] :only
+    #   Group(s) to include which will cause any dependencies marked as a member of the
+    #   group to be installed and all others to be ignored
+    def vendor(destination, options = {})
+      destination = File.expand_path(destination)
+      scratch     = Berkshelf.mktmpdir
+      chefignore  = nil
+
+      FileUtils.mkdir_p(destination)
+      FileUtils.remove_dir(destination, force: true)
+
+      unless (ignore_file = Berkshelf::Chef::Cookbook::Chefignore.find_relative_to(Dir.pwd)).nil?
+        chefignore = Berkshelf::Chef::Cookbook::Chefignore.new(ignore_file)
+      end
+
+      install(options.slice(:except, :only)).each do |cookbook|
+        Berkshelf.formatter.vendor(cookbook, destination)
+        cookbook_destination = File.join(scratch, cookbook.cookbook_name, '/')
+        FileUtils.mkdir_p(cookbook_destination)
+
+        # Dir.glob does not support backslash as a File separator
+        src   = cookbook.path.to_s.gsub('\\', '/')
+        files = Dir.glob(File.join(src, '*'))
+
+        if chefignore
+          files = chefignore.remove_ignores_from(files)
+        end
+
+        FileUtils.cp_r(files, cookbook_destination)
+      end
+
+      FileUtils.mv(scratch, destination)
+      destination
+    end
+
     # Get the lockfile corresponding to this Berksfile. This is necessary because
     # the user can specify a different path to the Berksfile. So assuming the lockfile
     # is named "Berksfile.lock" is a poor assumption.

lib/berkshelf/cli.rb
@@ -392,6 +392,26 @@ module Berkshelf
       berksfile.package(name, options)
     end
 
+    method_option :except,
+      type: :array,
+      desc: 'Exclude cookbooks that are in these groups.',
+      aliases: '-e'
+    method_option :only,
+      type: :array,
+      desc: 'Only cookbooks that are in these groups.',
+      aliases: '-o'
+    method_option :berksfile,
+      type: :string,
+      default: Berkshelf::DEFAULT_FILENAME,
+      desc: 'Path to a Berksfile to operate off of.',
+      aliases: '-b',
+      banner: 'PATH'
+    desc "vendor", "stuff"
+    def vendor(path)
+      berksfile = ::Berkshelf::Berksfile.from_file(options[:berksfile])
+      berksfile.vendor(path, options)
+    end
+
     desc 'version', 'Display version and copyright information'
     def version
       Berkshelf.formatter.msg version_header

lib/berkshelf/formatters.rb
@@ -79,7 +79,7 @@ module Berkshelf
           end
       end
 
-      formatter_methods :fetch, :install, :use, :upload, :msg, :error, :package, :show
+      formatter_methods :fetch, :install, :use, :upload, :msg, :error, :package, :show, :vendor
 
       def cleanup_hook
         # run after the task is finished

lib/berkshelf/formatters/human_readable.rb
@@ -54,6 +54,15 @@ module Berkshelf
         Berkshelf.ui.info(cookbook.pretty_print)
       end
 
+      # Output Cookbook vendor info message using {Berkshelf.ui}
+      #
+      # @param [CachedCookbook] cookbook
+      # @param [String] destination
+      def vendor(cookbook, destination)
+        cookbook_destination = File.join(destination, cookbook.cookbook_name)
+        Berkshelf.ui.info "Vendoring #{cookbook.cookbook_name} (#{cookbook.version}) to #{cookbook_destination}"
+      end
+
       # Output a generic message using {Berkshelf.ui}
       #
       # @param [String] message

lib/berkshelf/formatters/json.rb
@@ -89,6 +89,15 @@ module Berkshelf
         cookbooks[cookbook.cookbook_name] = cookbook.pretty_hash
       end
 
+      # Add a vendor message to delayed output
+      #
+      # @param [CachedCookbook] cookbook
+      # @param [String] destination
+      def vendor(cookbook, destination)
+        cookbook_destination = File.join(destination, cookbook.cookbook_name)
+        msg("Vendoring #{cookbook.cookbook_name} (#{cookbook.version}) to #{cookbook_destination}")
+      end
+
       # Add a generic message entry to delayed output
       #
       # @param [String] message

commit 179d105d4bfa12f374dd0e10d7ea8527b03542e4

fix chefignore for files in sub directories
It was broken since berkshelf 3.0.0:
1bfc97d64d38443e3326e85bd002c48399739c34 introduced the `vendor` command with the bug, and a2fcb18f41958af2b44e0b23547aa8cd07c24d8e re-implemented the `package` command using the bugged `vendor` one.

The chefignore patterns were only applied to the direct files and directories of the root, not in sub directories. Now we list all files (and empty directories) then apply the chefignore to this list.

Also add a Scenario for the command vendor feature to specifically test this.

This fixes issue #1317.

features/commands/vendor.feature
@@ -109,3 +109,23 @@ Feature: Vendoring cookbooks to a directory
     When I successfully run `berks vendor path/to/cukebooks`
     Then the directory "path/to/cukebooks/fake" should contain version "1.0.0" of the "fake" cookbook
 
+  Scenario: vendoring with a chefignore file and an excluded file in a sub directory
+    Given a cookbook named "chefignore_subdirectories"
+    And I cd to "chefignore_subdirectories"
+    And I have a Berksfile pointing at the local Berkshelf API with:
+      """
+      metadata
+      """
+    And I write to "chefignore" with:
+      """
+      *~
+      """
+    And an empty file named "test0"
+    And an empty file named "test1~"
+    And an empty file named "subdir/test2~"
+    When I successfully run `berks vendor vendor`
+    And I cd to "vendor/chefignore_subdirectories"
+    And a file named "test0" should exist
+    And a file named "test1~" should not exist
+    And a file named "subdir/test2~" should not exist
+

lib/berkshelf/berksfile.rb
@@ -587,7 +587,10 @@ module Berkshelf
 
           # Dir.glob does not support backslash as a File separator
           src   = cookbook.path.to_s.gsub('\\', '/')
-          files = FileSyncer.glob(File.join(src, '*'))
+          # List all files and empty directories recursively
+          files = FileSyncer.glob(File.join(src, '**/*')).reject do |dir|
+            File.directory?(dir) and Dir.entries(dir) != [".", ".."]
+          end
 
           chefignore = Ridley::Chef::Chefignore.new(cookbook.path.to_s) rescue nil
           chefignore.apply!(files) if chefignore