a961f08e7bf8a392d2c27cce57a031f243741187
Joshua Smith <kernelsmith@metasploit.com>
2016-01-28 00:25:00 -0600

    adds github.transport to config.json

--- lib/berkshelf/config.rb
+++ lib/berkshelf/config.rb
@@ -139,5 +139,10 @@ module Berkshelf
       type: Array,
       default: [],
       required: false
+    attribute 'github.transport',
+      # git, ssh, or https
+      type: String,
+      default: 'git',
+      required: false
   end
 end
