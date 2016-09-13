753da17951c11843ae5ff2562bc1e5f2d2696d02
Matt Wrock <matt@mattwrock.com>
2016-09-13 12:28:54 -0700

    fix cucumber tests

--- features/step_definitions/cli_steps.rb
+++ features/step_definitions/cli_steps.rb
@@ -2,3 +2,17 @@ Then /^the exit status should be "(.+)"$/ do |name|
   error = name.split('::').reduce(Berkshelf) { |klass, id| klass.const_get(id) }
   expect(last_command_started).to have_exit_status(error.status_code)
 end
+
+Then /^the results should have the cookbooks:$/ do |cookbooks|
+  list = last_command_started.stdout
+  cookbooks.split("\n").each do |cookbook|
+    expect(list).to  include(cookbook)
+  end
+end
+
+Then /^the results should each start with "(.+)"$/ do |prefix|
+  list = last_command_started.stdout
+  list.split("\n").each do |cookbook|
+    expect(cookbook).to  start_with(prefix)
+  end
+end
