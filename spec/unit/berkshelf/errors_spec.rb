c3004846cfcccf67997b21510769b372d8073c6f
tkling <tyler.kling@careerbuilder.com>
2016-10-10 16:42:13 -0400

    assert message and api_uri appear in error text

--- spec/unit/berkshelf/errors_spec.rb
+++ spec/unit/berkshelf/errors_spec.rb
@@ -3,3 +3,18 @@ require 'spec_helper'
 describe Berkshelf::BerkshelfError do
   skip
 end
+
+describe Berkshelf::CommunitySiteError do
+  let(:api_uri) { 'https://infra.as.code' }
+  let(:message) { 'Cookbook name' }
+
+  subject { described_class.new(api_uri, message) }
+
+  it "includes the supplied uri in the error message" do
+    expect(subject.message).to include api_uri
+  end
+
+  it "includes the supplied message in the error message" do
+    expect(subject.message).to include message
+  end
+end
