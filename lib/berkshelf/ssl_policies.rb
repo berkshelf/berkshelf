2f63392879be936562638ea257ab53447d27f1e5
Salim Afiune <afiune@chef.io>
2016-12-07 20:36:13 -0500

    Added tests for SSLPolicy and cleaned code
    Signed-off-by: Salim Afiune <afiune@chef.io>

--- lib/berkshelf/ssl_policies.rb
+++ lib/berkshelf/ssl_policies.rb
@@ -1,49 +1,40 @@
 require 'openssl'
-require 'pathname'
 
 module Berkshelf
   class SSLPolicy
+
+    # @return [Store]
+    #   Holds trusted CA certificates used to verify peer certificates
     attr_reader :store
 
     def initialize
-      set_store
-    end
-
-    def set_store
-      if ::File.exist?(trusted_certs_dir)
-        @store = OpenSSL::X509::Store.new.tap do |store|
-          store.set_default_paths
-        end
-
-        self.set_custom_certs
-      else
-        @store = nil
-      end
-    end
-
-    def trusted_certs_dir
-      config_dir = Berkshelf.config.chef.trusted_certs_dir.to_s
-      if config_dir.empty? && !::File.exist?(config_dir)
-        trusted_certs_dir = "#{ENV['HOME']}/.chef/trusted_certs"
-      else
-        trusted_certs_dir = config_dir
+      @store = OpenSSL::X509::Store.new.tap do |store|
+        store.set_default_paths
       end
 
-      trusted_certs_dir
-    end
-
-    def set_custom_certs
-      certs = Pathname.glob("#{trusted_certs_dir}/" "{*.crt,*.pem}")
-      certs.each do |cert|
-        cert = OpenSSL::X509::Certificate.new(IO.read(cert))
-        add_trusted_cert(cert)
-      end
+      set_custom_certs if ::File.exist?(trusted_certs_dir)
     end
 
     def add_trusted_cert(cert)
       @store.add_cert(cert)
     rescue OpenSSL::X509::StoreError => e
       raise e unless e.message == 'cert already in hash table'
+    end
+
+    def trusted_certs_dir
+      config_dir = Berkshelf.config.chef.trusted_certs_dir.to_s
+      if config_dir.empty? || !::File.exist?(config_dir)
+        File.join(ENV['HOME'], '.chef', 'trusted_certs')
+      else
+        config_dir
+      end
+    end
+
+    def set_custom_certs
+      ::Dir.glob("#{trusted_certs_dir}/" "{*.crt,*.pem}").each do |cert|
+        cert = OpenSSL::X509::Certificate.new(IO.read(cert))
+        add_trusted_cert(cert)
+      end
     end
   end
 end
---
+++ spec/unit/berkshelf/ssl_policies_spec.rb
@@ -0,0 +1,75 @@
+require 'spec_helper'
+
+describe Berkshelf::SSLPolicy do
+  let(:self_signed_crt_path) { File.join(BERKS_SPEC_DATA, 'trusted_certs') }
+
+  let(:chef_config) do
+    double(Ridley::Chef::Config,
+      node_name: 'fake-client',
+      client_key: 'client-key',
+      chef_server_url: 'http://configured-chef-server/',
+      validation_client_name: 'validator',
+      validation_key: 'validator.pem',
+      cookbook_copyright: 'user',
+      cookbook_email: 'user@example.com',
+      cookbook_license: 'apachev2',
+      trusted_certs_dir: self_signed_crt_path,
+      knife: {
+        chef_guard: false
+      }
+    )
+  end
+
+  let(:berkshelf_config) do
+    double(Berkshelf::Config,
+      ssl:  double(verify: true),
+      chef: chef_config
+    )
+  end
+
+  subject do
+    Berkshelf::SSLPolicy.new()
+  end
+
+  before do
+    allow(Berkshelf).to receive(:config).and_return(berkshelf_config)
+  end
+
+  describe '#initialize' do
+    it 'sets up the store' do
+      expect(subject.store.class).to be(OpenSSL::X509::Store)
+    end
+
+    it 'sets up custom certificates for chef' do
+    end
+  end
+
+  describe '#trusted_certs_dir' do
+    it 'uses the trusted_certs_dir from Berkshelf config' do
+        expect(subject.trusted_certs_dir).to eq(self_signed_crt_path)
+    end
+
+    context 'trusted_certs_dir in Berkshelf' do
+
+      context 'config is not set' do
+        before { allow(chef_config).to receive_messages(trusted_certs_dir: nil) }
+
+        it 'defaults to ~/.chef/trusted_certs' do
+          expect(subject.trusted_certs_dir).to eq(
+            File.join(ENV['HOME'], '.chef', 'trusted_certs')
+          )
+        end
+      end
+
+      context 'config is seti but does not exist' do
+        before { allow(chef_config).to receive_messages(trusted_certs_dir: '/fake') }
+
+        it 'defaults to ~/.chef/trusted_certs' do
+          expect(subject.trusted_certs_dir).to eq(
+            File.join(ENV['HOME'], '.chef', 'trusted_certs')
+          )
+        end
+      end
+    end
+  end
+end
