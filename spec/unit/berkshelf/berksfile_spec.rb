f642c3b31bf3b0662de78f31e58d2e872dc6a58c
Lamont Granquist <lamont@scriptkiddie.org>
2017-01-24 13:50:03 -0800

    Revert "fix specs"
    This reverts commit 4fb6a417b497413ca1b65f492c38fd0fe5dbbb6a.

--- spec/unit/berkshelf/berksfile_spec.rb
+++ spec/unit/berkshelf/berksfile_spec.rb
@@ -387,9 +387,10 @@ describe Berkshelf::Berksfile do
   describe '#vendor' do
     let(:cached_cookbook)    { double(Berkshelf::CachedCookbook, cookbook_name: 'my_cookbook', path: '/my_cookbook/path', compiled_metadata?: true) }
     let(:installer)          { double(Berkshelf::Installer, run: [cached_cookbook]) }
+    let(:raw_metadata_files) { [File::join(cached_cookbook.cookbook_name, 'metadata.rb')] }
 
     let(:destination) { '/a/destination/path' }
-    let(:options)    { { :exclude => Berkshelf::Berksfile::EXCLUDED_VCS_FILES_WHEN_VENDORING, delete: nil } }
+    let(:options)    { { :exclude => raw_metadata_files + Berkshelf::Berksfile::EXCLUDED_VCS_FILES_WHEN_VENDORING, delete: nil } }
 
     before do
       allow(Berkshelf::Installer).to receive(:new).and_return(installer)
@@ -401,9 +402,9 @@ describe Berkshelf::Berksfile do
       subject.vendor(destination)
     end
 
-    it 'includes the top-level metadata.rb file' do
+    it 'excludes the top-level metadata.rb file' do
       expect(options[:exclude].any? { |exclude| File.fnmatch?(exclude, 'my_cookbook/recipes/metadata.rb', File::FNM_DOTMATCH) }).to be(false)
-      expect(options[:exclude].any? { |exclude| File.fnmatch?(exclude, 'my_cookbook/metadata.rb', File::FNM_DOTMATCH) }).to be(false)
+      expect(options[:exclude].any? { |exclude| File.fnmatch?(exclude, 'my_cookbook/metadata.rb', File::FNM_DOTMATCH) }).to be(true)
     end
   end
 
