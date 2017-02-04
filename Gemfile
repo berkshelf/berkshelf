3a157f94df8662223ca0de782097073d9a620c46
Bill Ruddock <bill.ruddock@gmail.com>
2017-02-04 16:38:36 +0000

    Remove spork
    Spork is no longer maintained and cucumber >= 1.3.0 dropped support for it
    
    Also change Gemfile to use install_if for platform-specific gems, keeping
    Gemfile.lock consistent.

--- Gemfile
+++ Gemfile
@@ -11,23 +11,22 @@ group :guard do
   gem "guard"
   gem "guard-cucumber"
   gem "guard-rspec"
-  gem "guard-spork"
 
   require "rbconfig"
 
-  if RbConfig::CONFIG["target_os"] =~ /darwin/i
+  install_if -> { RbConfig::CONFIG["target_os"] =~ /darwin/i } do
     gem "growl", require: false
     gem "rb-fsevent", require: false
 
-    if `uname`.strip == "Darwin" && `sw_vers -productVersion`.strip >= "10.8"
+    install_if -> { `uname`.strip == "Darwin" && `sw_vers -productVersion`.strip >= "10.8" } do
       gem "terminal-notifier-guard", "~> 1.5.3", require: false
     end rescue Errno::ENOENT
-
-  elsif RbConfig::CONFIG["target_os"] =~ /linux/i
+  end
+  install_if -> {RbConfig::CONFIG["target_os"] =~ /linux/i } do
     gem "libnotify",  "~> 0.8.0", require: false
     gem "rb-inotify", require: false
-
-  elsif RbConfig::CONFIG["target_os"] =~ /mswin|mingw/i
+  end
+  install_if -> { RbConfig::CONFIG["target_os"] =~ /mswin|mingw/i } do
     gem "rb-notifu", ">= 0.0.4", require: false
     gem "wdm", require: false
     gem "win32console", require: false
@@ -46,7 +45,6 @@ group :development do
   gem "dep_selector",  ">= 1.0"
   gem "fuubar",        ">= 2.0"
   gem "rspec",         ">= 3.0"
-  gem "spork",         ">= 0.9"
   gem "test-kitchen",  ">= 1.2"
   gem "webmock",       ">= 1.11"
   gem "yard",          ">= 0.8"
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,82 +1,76 @@
-require "spork"
-
 def windows?
   !!(RUBY_PLATFORM =~ /mswin|mingw|windows/)
 end
 
-Spork.prefork do
-  require "aruba/cucumber"
-  require "aruba/in_process"
-  require "aruba/spawn_process"
-  require "cucumber/rspec/doubles"
-  require "berkshelf/api/rspec" unless windows?
-  require "berkshelf/api/cucumber" unless windows?
+require "aruba/cucumber"
+require "aruba/in_process"
+require "aruba/spawn_process"
+require "cucumber/rspec/doubles"
+require "berkshelf/api/rspec" unless windows?
+require "berkshelf/api/cucumber" unless windows?
 
-  Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
+Dir["spec/support/**/*.rb"].each { |f| require File.expand_path(f) }
 
-  World(Berkshelf::RSpec::PathHelpers)
-  World(Berkshelf::RSpec::Kitchen)
+World(Berkshelf::RSpec::PathHelpers)
+World(Berkshelf::RSpec::Kitchen)
 
-  CHEF_SERVER_PORT = 26310
-  BERKS_API_PORT   = 26210
+CHEF_SERVER_PORT = 26310
+BERKS_API_PORT   = 26210
 
-  at_exit do
-    Berkshelf::RSpec::ChefServer.stop
-    Berkshelf::API::RSpec::Server.stop unless windows?
-  end
+at_exit do
+  Berkshelf::RSpec::ChefServer.stop
+  Berkshelf::API::RSpec::Server.stop unless windows?
+end
 
-  Before do
+Before do
 
-    # Legacy ENV variables until we can move over to all InProcess
-    Berkshelf.instance_variable_set(:@berkshelf_path, nil)
-    ENV["BERKSHELF_PATH"] = berkshelf_path.to_s
-    ENV["BERKSHELF_CONFIG"] = Berkshelf.config.path.to_s
-    ENV["BERKSHELF_CHEF_CONFIG"] = chef_config_path.to_s
+  # Legacy ENV variables until we can move over to all InProcess
+  Berkshelf.instance_variable_set(:@berkshelf_path, nil)
+  ENV["BERKSHELF_PATH"] = berkshelf_path.to_s
+  ENV["BERKSHELF_CONFIG"] = Berkshelf.config.path.to_s
+  ENV["BERKSHELF_CHEF_CONFIG"] = chef_config_path.to_s
 
-    aruba.config.command_launcher = :in_process
-    aruba.config.main_class = Berkshelf::Cli::Runner
-    @dirs = ["spec/tmp/aruba"] # set aruba's temporary directory
+  aruba.config.command_launcher = :in_process
+  aruba.config.main_class = Berkshelf::Cli::Runner
+  @dirs = ["spec/tmp/aruba"] # set aruba's temporary directory
 
-    stub_kitchen!
-    clean_tmp_path
-    Berkshelf.initialize_filesystem
-    Berkshelf::CookbookStore.instance.initialize_filesystem
-    reload_configs
-    Berkshelf::CachedCookbook.instance_variable_set(:@loaded_cookbooks, nil)
+  stub_kitchen!
+  clean_tmp_path
+  Berkshelf.initialize_filesystem
+  Berkshelf::CookbookStore.instance.initialize_filesystem
+  reload_configs
+  Berkshelf::CachedCookbook.instance_variable_set(:@loaded_cookbooks, nil)
 
-    endpoints = [
-      {
-        type: "chef_server",
-        options: {
-          url: "http://localhost:#{CHEF_SERVER_PORT}",
-          client_name: "reset",
-          client_key: File.expand_path("spec/config/berkshelf.pem"),
-        },
+  endpoints = [
+    {
+      type: "chef_server",
+      options: {
+        url: "http://localhost:#{CHEF_SERVER_PORT}",
+        client_name: "reset",
+        client_key: File.expand_path("spec/config/berkshelf.pem"),
       },
-    ]
+    },
+  ]
 
-    Berkshelf::RSpec::ChefServer.start(port: CHEF_SERVER_PORT)
-    Berkshelf::API::RSpec::Server.start(port: BERKS_API_PORT, endpoints: endpoints) unless windows?
+  Berkshelf::RSpec::ChefServer.start(port: CHEF_SERVER_PORT)
+  Berkshelf::API::RSpec::Server.start(port: BERKS_API_PORT, endpoints: endpoints) unless windows?
 
-    aruba.config.io_wait_timeout = Cucumber::JRUBY ? 7 : 5
-    @aruba_timeout_seconds = Cucumber::JRUBY ? 35 : 15
-  end
-
-  Before("@spawn") do
-    aruba.config.command_launcher = :spawn
-
-    Berkshelf.instance_variable_set(:@berkshelf_path, nil)
-    set_environment_variable("BERKSHELF_PATH", berkshelf_path.to_s)
-    set_environment_variable("BERKSHELF_CONFIG", Berkshelf.config.path.to_s)
-    set_environment_variable("BERKSHELF_CHEF_CONFIG", chef_config_path.to_s)
-  end
-
-  Before("@slow_process") do
-    aruba.config.io_wait_timeout = Cucumber::JRUBY ? 70 : 30
-    @aruba_timeout_seconds = Cucumber::JRUBY ? 140 : 60
-  end
+  aruba.config.io_wait_timeout = Cucumber::JRUBY ? 7 : 5
+  @aruba_timeout_seconds = Cucumber::JRUBY ? 35 : 15
 end
 
-Spork.each_run do
-  require "berkshelf/cli"
+Before("@spawn") do
+  aruba.config.command_launcher = :spawn
+
+  Berkshelf.instance_variable_set(:@berkshelf_path, nil)
+  set_environment_variable("BERKSHELF_PATH", berkshelf_path.to_s)
+  set_environment_variable("BERKSHELF_CONFIG", Berkshelf.config.path.to_s)
+  set_environment_variable("BERKSHELF_CHEF_CONFIG", chef_config_path.to_s)
 end
+
+Before("@slow_process") do
+  aruba.config.io_wait_timeout = Cucumber::JRUBY ? 70 : 30
+  @aruba_timeout_seconds = Cucumber::JRUBY ? 140 : 60
+end
+
+require "berkshelf/cli"
