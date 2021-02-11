091df26d6e032d883cb8e8a00cf31592b81d5a4b
Lamont Granquist <lamont@scriptkiddie.org>
2021-02-10 16:13:01 -0800

    Bump mixlib-archive for Ruby 3.0 fix
    This should get berkshelf green for ruby-3.0
    
    Also breaks out specs+cukes into different runs so its easier to see
    what is exploding across tests
    
    Adds a utility helper for getting debug output from berkshelf commands
    run inside of cukes
    
    Signed-off-by: Lamont Granquist <lamont@scriptkiddie.org>

--- features/step_definitions/utility_steps.rb
+++ features/step_definitions/utility_steps.rb
@@ -9,3 +9,9 @@ Then /the output from \`(.+)\` should be the same as \`(.+)\`/ do |actual, expec
   expected_output = last_command_started.stdout
   expect(actual_output).to eql(expected_output)
 end
+
+When(/^I run `(.*?)`(?: for up to ([\d.]+) seconds)? printing output$/) do |cmd, secs|
+  cmd = sanitize_text(cmd)
+  cmd = run_command(cmd, :fail_on_error => true, :exit_timeout => secs && secs.to_f)
+  puts cmd.output
+end
