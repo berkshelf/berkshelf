b390c09f9cfa347888bf7cf8b33033735f0358ec
Pranay Singh <i5singh.pranay@gmail.com>
2022-06-01 17:55:33 +0530

    create a package with relative path
    Signed-off-by: Pranay Singh <i5singh.pranay@gmail.com>

--- features/commands/package.feature
+++ features/commands/package.feature
@@ -15,3 +15,20 @@ Feature: berks package
       """
       Cookbook(s) packaged to
       """
+    And the archive "my-cookbooks.tar.gz" should contain:
+      """
+      cookbooks
+      cookbooks/fake
+      cookbooks/fake/attributes
+      cookbooks/fake/attributes/default.rb
+      cookbooks/fake/files
+      cookbooks/fake/files/default
+      cookbooks/fake/files/default/file.h
+      cookbooks/fake/metadata.json
+      cookbooks/fake/metadata.rb
+      cookbooks/fake/recipes
+      cookbooks/fake/recipes/default.rb
+      cookbooks/fake/templates
+      cookbooks/fake/templates/default
+      cookbooks/fake/templates/default/template.erb
+      """
--- lib/berkshelf/packager.rb
+++ lib/berkshelf/packager.rb
@@ -1,4 +1,5 @@
 require "archive/tar/minitar"
+require "find" unless defined?(Find)
 require "zlib" unless defined?(Zlib)
 
 module Berkshelf
@@ -40,8 +41,18 @@ module Berkshelf
     # @return [String]
     #   path to the generated archive
     def run(source)
-      tgz = Zlib::GzipWriter.new(File.open(out_file, "wb"))
-      Archive::Tar::Minitar.pack(Dir.glob("#{source}/*"), tgz)
+      begin
+        dest = Zlib::GzipWriter.open(out_file)
+        tar = RelativeTarWriter.new(dest, source)
+        Find.find(source) do |entry|
+          next if source == entry
+
+          Archive::Tar::Minitar.pack_file(entry, tar)
+        end
+      ensure
+        tar.close
+        dest.close
+      end
 
       out_file
     rescue SystemCallError => ex
@@ -67,5 +78,30 @@ module Berkshelf
 
     # @return [String]
     attr_reader :filename
+
+    # A private decorator for Archive::Tar::Minitar::Writer that
+    # turns absolute paths into relative ones.
+    class RelativeTarWriter < SimpleDelegator #:nodoc:
+      def initialize(io, base_path)
+        @base_path = Pathname.new(base_path)
+        super(Archive::Tar::Minitar::Writer.new(io))
+      end
+
+      %w{add_file add_file_simple mkdir}.each do |method|
+        class_eval <<~RUBY
+          def #{method}(name, *opts, &block)
+            super(relative_path(name), *opts, &block)
+          end
+        RUBY
+      end
+
+      private
+
+      attr_reader :base_path
+
+      def relative_path(path)
+        Pathname.new(path).relative_path_from(base_path).to_s
+      end
+    end
   end
 end
